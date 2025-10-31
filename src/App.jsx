6ae0f15b392c60390d066e4d4ac4958d8b095478
Tung Vu Nguyen <tungvunguyennguyen@gmail.com>
2025-10-31 02:13:40 +0000

    do i need a license key from tldraw?

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Tldraw } from '@tldraw/tldraw'
 import './App.css'
 
 function App() {
+  const licenseKey = import.meta.env.VITE_TLDRAW_LICENSE_KEY
+
   return (
     <div className="tldraw-wrapper">
-      <Tldraw autoFocus />
+      <Tldraw autoFocus licenseKey={licenseKey} />
     </div>
   )
 }
