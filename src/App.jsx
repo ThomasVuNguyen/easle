1d5b6c2d8ce841db0b9b4933dae78667678a776b
Tung Vu Nguyen <tungvunguyennguyen@gmail.com>
2025-10-31 02:18:32 +0000

    the old tldraw kinda sucks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import '@tldraw/tldraw/tldraw.css'
 import { Tldraw } from '@tldraw/tldraw'
 import './App.css'
 
 function App() {
-  const licenseKey = import.meta.env.VITE_TLDRAW_LICENSE_KEY
-
   return (
     <div className="tldraw-wrapper">
-      <Tldraw autoFocus licenseKey={licenseKey} />
+      <Tldraw />
     </div>
   )
 }
